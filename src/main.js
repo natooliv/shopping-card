91be1e74052648dc27e7a0c21c805485149721d2
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2022-11-16 18:25:24 -0300

    Item 4

--- src/main.js
+++ src/main.js
@@ -3,7 +3,23 @@ import './style.css';
 import { createProductElement } from './helpers/shopFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 
+document.querySelector('.cep-button').addEventListener('click', searchCep);
+const carregandoo = document.querySelector('#carregando');
+
+const carregando = () => {
+  const criandoElemento = document.createElement('h1');
+  criandoElemento.className = 'loading';
+  criandoElemento.innerText = 'carregando...';
+  carregandoo.appendChild(criandoElemento);
+};
+
+const removeCarregando = () => {
+  const carregando2 = document.querySelector('.loading');
+  carregando2.remove();
+};
+
 const criandoItem = async (param) => {
+  carregando();
   const produtos = await fetchProductsList(param);
   console.log(produtos);
   const criandoMaisUm = document.querySelector('.products');
@@ -13,9 +29,9 @@ const criandoItem = async (param) => {
       createProductElement({ id, title, thumbnail, price })
     );
   });
+  removeCarregando();
 };
+
 window.onload = async () => {
   await criandoItem('computador');
 };
-
-document.querySelector('.cep-button').addEventListener('click', searchCep);
