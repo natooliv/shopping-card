bde0a6290bb2aa0cde5c846765d70af5dbd29451
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2022-11-16 18:12:01 -0300

    Item 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+
+const criandoItem = async (param) => {
+  const produtos = await fetchProductsList(param);
+  console.log(produtos);
+  const criandoMaisUm = document.querySelector('.products');
+  produtos.forEach((item) => {
+    const { id, title, thumbnail, price } = item;
+    criandoMaisUm.appendChild(
+      createProductElement({ id, title, thumbnail, price })
+    );
+  });
+};
+window.onload = async () => {
+  await criandoItem('computador');
+};
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
